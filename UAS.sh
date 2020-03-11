7c54d06c63f6dba88239eae9d3226058ed7dd490
Dagsi <39049329+Dagsi@users.noreply.github.com>
2020-03-11 16:26:15 +0800

    Update UAS.sh

--- UAS.sh
+++ UAS.sh
@@ -73,6 +73,36 @@ iptables -I INPUT -s 106.11.222.64/26 -j DROP
 iptables -I INPUT -s 106.11.222.128/26 -j DROP
 iptables -I INPUT -s 106.11.222.192/26 -j DROP
 iptables -I INPUT -s 106.11.223.0/26 -j DROP
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+iptables -I INPUT -s 140.205.225.195/32 -j DROP
+iptables -I INPUT -s 140.205.225.204/32 -j DROP
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+iptables -I INPUT -s 140.205.225.195/32 -j DROP
+iptables -I INPUT -s 140.205.225.204/32 -j DROP
+iptables -I INPUT -s 106.11.224.0/26 -j DROP
+iptables -I INPUT -s 106.11.224.64/26 -j DROP
+iptables -I INPUT -s 106.11.224.128/26 -j DROP
+iptables -I INPUT -s 106.11.224.192/26 -j DROP
+iptables -I INPUT -s 106.11.222.64/26 -j DROP
+iptables -I INPUT -s 106.11.222.128/26 -j DROP
+iptables -I INPUT -s 106.11.222.192/26 -j DROP
+iptables -I INPUT -s 106.11.223.0/26 -j DROP
 service iptables save
 rm -rf /etc/motd
 wget -O /etc/motd -N --no-check-certificate https://raw.githubusercontent.com/123wbx321wbx/Uninstall-aliyun-service/master/Other/motd
